05fc102fc9eb22b4bb7c91562591e223fac131e7
Khaerul Azmi <khaerulazmi882@gmail.com>
2025-10-21 21:57:38 +0700

    ubah icon

--- storage/framework/views/b54434056ecbdbfaee2e4561f59eb6c3.php
+++ storage/framework/views/b54434056ecbdbfaee2e4561f59eb6c3.php
@@ -124,9 +124,23 @@
 <?php endif; ?>
 
         <button type="button"
-            class="toggle-password absolute right-3 top-1/2 -translate-y-1/2 flex items-center justify-center text-gray-500 hover:text-gray-700 focus:outline-none">
-            <svg class="icon-eye w-5 h-5" ...>...</svg>
-            <svg class="icon-eye-off w-5 h-5 hidden" ...>...</svg>
+            class="toggle-password absolute right-3 top-1/2 -translate-y-1/2 flex items-center justify-center text-gray-600 hover:text-indigo-600 focus:outline-none">
+            
+            <!-- Ikon "lihat" -->
+            <svg class="icon-eye w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+            </svg>
+
+            <!-- Ikon "tutup" -->
+            <svg class="icon-eye-off w-5 h-5 hidden" xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7a10.05 10.05 0 012.68-4.362M9.88 9.88a3 3 0 104.24 4.24M3 3l18 18" />
+            </svg>
         </button>
     </div>
 
@@ -151,6 +165,7 @@
 <?php unset($__componentOriginalf94ed9c5393ef72725d159fe01139746); ?>
 <?php endif; ?>
 </div>
+
 
             <!-- Remember Me + Forgot Password -->
             <div class="flex items-center justify-between mb-4">
